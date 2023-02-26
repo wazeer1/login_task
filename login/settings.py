4e8aebf98ee1273a7f0594d8d0a67961c7a7a4e2
wazeer_ahmd <wazeer@talrop.com>
2023-02-26 17:42:47 +0530

    1

--- login/settings.py
+++ login/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-p=-y7=z-_t7i+m-%lrrq^mj1yfpj08j-#*hw(uaa@*f$o9!&00
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh','https://login-meshinetest1.onrender.com/']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','login-meshinetest1.onrender.com']
 
 ENCRYPT_KEY = "-WLfCkjZS2ms3kTFNQbOGsh2RpyNX2mhvgL8PYIx3vA="
 # Application definition
@@ -55,7 +55,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     '.vercel.app', 
     '.now.sh',
-    'https://login-meshinetest1.onrender.com/',
+    'https://login-meshinetest1.onrender.com',
 ]
 
 MIDDLEWARE = [
