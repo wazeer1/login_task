f56b620bbdd975cc2a33e708990d03919b8faf1a
wazeer_ahmd <wazeer@talrop.com>
2023-02-26 14:45:34 +0530

    vercel config

--- login/settings.py
+++ login/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = "django-insecure-p=-y7=z-_t7i+m-%lrrq^mj1yfpj08j-#*hw(uaa@*f$o9!&00
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 ENCRYPT_KEY = "-WLfCkjZS2ms3kTFNQbOGsh2RpyNX2mhvgL8PYIx3vA="
 # Application definition
@@ -52,6 +53,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://sub.example.com",
     "http://localhost:8001",
     "http://localhost:3000",
+    '.vercel.app', 
+    '.now.sh',
 ]
 
 MIDDLEWARE = [
@@ -165,9 +168,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_URL = "static/"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
